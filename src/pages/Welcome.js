ed908acf80708c1774f33db628f9753ad0041087
Selina Lin <selina.lin@ufl.edu>
2025-02-20 10:53:41 -0500

    Banner for welcome page set up

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -17,9 +17,15 @@ function Welcome() {
   return (
     <div className="blank-page">
       <Header />
-      <h1 className="welcome-heading">We make safety as a priority at the University of Florida!</h1>
-      <button className="welcome-button" onClick={handleGuestClick}>Use As Guest</button>
-      <button className="nav-button" onClick={handleSignInClick}>Sign in using GatorLink</button>
+      {/* Banner Section */}
+      <div className="welcome-banner">
+        <h1 className="welcome-heading">We make safety as a priority at the University of Florida!</h1>
+        <div className="welcome-buttons">
+          <button className="welcome-button" onClick={handleGuestClick}>Use As Guest</button>
+          <button className="nav-button" onClick={handleSignInClick}>Sign in using GatorLink</button>
+        </div>
+      </div>
+      {/* Paragraphs */}
       <p className="welcome-paragraph1">Obtain step-by-step guides for emergency situations</p>
       <p className="welcome-paragraph2">Access safety resources provided by UF and curated by our team</p>
       <p className="welcome-paragraph3">Find assistance for health, legal, counseling and academic support</p>
